081473553fa56ba77285505751245a8691b63e1d
zading <1105503089@qq.com>
2023-11-22 14:28:44 +0800

    feat(pie): add pie chart and donut chart type

---
+++ src/lib/pie/pie.options.ts
@@ -0,0 +1,13 @@
+import { PieOptions } from './pie.types';
+
+const defaultPieChartOptions: PieOptions = {
+  legend: {
+    display: true,
+    position: 'right',
+  },
+  doughnutLabel: {
+    enable: false,
+  },
+};
+
+export { defaultPieChartOptions };
---
+++ src/lib/pie/pie.plugins.ts
@@ -0,0 +1,52 @@
+import { Chart, LegendItem } from 'chart.js/auto';
+import { DoughnutLabel } from './pie.types';
+
+const getCenterValue = (chart: Chart | any, total: number, label?: string | number) => {
+  const ctx = chart.ctx;
+  const centerY = chart.height / 2;
+
+  const innerRadius = chart.getDatasetMeta(0).data[0].innerRadius;
+  const outerRadius = chart.getDatasetMeta(0).data[0].outerRadius;
+
+  let scaleNum = innerRadius / 120;
+  const valueFontSize = scaleNum < 1 ? 36 * scaleNum : 36;
+  const totalFontSize = scaleNum < 1 ? 14 * scaleNum : 14;
+
+  ctx.restore();
+  if (label) {
+    const totalText = label ? label : '';
+    ctx.font = `${totalFontSize}px Arial`;
+    ctx.textBaseline = 'middle';
+    ctx.fillStyle = 'black';
+    ctx.fillText(totalText, outerRadius - ctx.measureText(totalText).width / 2, centerY + valueFontSize);
+  }
+
+  ctx.font = `${valueFontSize}px Arial`;
+  ctx.fillText(total, outerRadius - ctx.measureText(total).width / 2, label ? centerY : centerY + valueFontSize / 2);
+
+  ctx.save();
+};
+
+const centerValue = (label?: DoughnutLabel) => {
+  return {
+    id: 'centerValue',
+    afterDatasetDraw: (chart: Chart | any) => {
+      if (!label?.enable) {
+        return;
+      }
+      const total = chart.getDatasetMeta(0).total;
+      getCenterValue(chart, total, label.label);
+    },
+  };
+};
+
+const legendClickHandler = (e: any, item: LegendItem, legend: any): void => {
+  const chart = legend.chart;
+  const index = item.index;
+  chart.toggleDataVisibility(index);
+  const total = chart.getDatasetMeta(0).total;
+  getCenterValue(chart, total);
+  chart.update();
+};
+
+export { centerValue, legendClickHandler };
