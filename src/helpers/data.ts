27ec46b18e891889d1ddf6460c27e4b64eff52f3
Bendy Zhang <zb@bndy.net>
2023-12-14 15:33:32 +0800

    style: lint and code format

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -74,7 +74,7 @@ export function isEmptyObject(value: Record<string, unknown>): boolean {
   return false;
 }
 
-export function getCombinedKeys(data: Record<string, any>[]): string[] {
+export function getCombinedKeys(data: Record<string, unknown>[]): string[] {
   const mergedKeys = new Set<string>();
   data.forEach((item) => {
     Object.keys(item).forEach((key) => {
