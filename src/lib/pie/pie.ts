82ef1fa7010c37f7086738deaa35bfd33959e3b4
zading <1105503089@qq.com>
2024-03-01 10:12:47 +0800

    fix(segment): fix integrate issue

--- src/lib/pie/pie.ts
+++ src/lib/pie/pie.ts
@@ -126,6 +126,7 @@ export class PieChart<TData extends PieData, TOptions extends PieChartOptions> e
       maintainAspectRatio: false,
       responsive: true,
       onClick: (event, elements, chart) => {
+        console.log('---onSegmentClick');
         this.segmentClick?.onSegmentClick(event, elements, chart);
       },
       plugins: {
