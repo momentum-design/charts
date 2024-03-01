4b89e1a0169084f49160c08f45383639d1293a8a
zading <1105503089@qq.com>
2024-03-01 13:59:35 +0800

    fix(segment): fix data is empty case

--- src/core/plugins/chart-event.ts
+++ src/core/plugins/chart-event.ts
@@ -1,15 +1,19 @@
 import type { Chart } from 'chart.js/auto';
 import { alphaColor } from '../../helpers/color';
+import { CJElement } from '../../types';
 
 export function chartSegmentStatus(originChartColor: string | string[]) {
   return {
     id: 'chartSegmentStatus',
     beforeUpdate: (chart: Chart): void => {
       const data = chart.config.data;
+      if (!data?.datasets?.length) {
+        return;
+      }
       const metaData = chart.getDatasetMeta(0);
       const originBG = originChartColor;
 
-      const selectedArr = metaData?.data?.map((data) => data.active);
+      const selectedArr = metaData?.data?.map((data: CJElement) => data?.selected ?? false);
 
       if (typeof originBG !== 'string' && originBG?.length > 0) {
         const result = originBG.map((color: string, index: number) => {
@@ -19,11 +23,11 @@ export function chartSegmentStatus(originChartColor: string | string[]) {
             return alphaColor(color, 0.4);
           }
         });
-        data.datasets.length ? (data.datasets[0].backgroundColor = result) : null;
+        data.datasets[0].backgroundColor = result;
       }
 
       if (selectedArr.every((selected) => selected === false)) {
-        data.datasets.length ? (data.datasets[0].backgroundColor = originBG) : null;
+        data.datasets[0].backgroundColor = originBG;
       }
     },
   };
