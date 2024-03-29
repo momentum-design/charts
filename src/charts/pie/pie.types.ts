b6945505b884f4c6afa3f6523673af4318a6bbc9
Zan DIng <37390608+justinZan@users.noreply.github.com>
2024-03-29 10:42:35 +0800

    docs(website): add pie and donut docs (#60)

--- src/charts/pie/pie.types.ts
+++ src/charts/pie/pie.types.ts
@@ -2,7 +2,6 @@ import { ChartOptions, JsonData, TableData } from '../../types';
 
 export interface PieChartOptions extends ChartOptions {
   innerRadius?: string | number;
-  additionalLabel?: string | number;
   dataKey?: string;
 }
 
