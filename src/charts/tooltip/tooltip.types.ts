53c73ddfe77f0f2811c5f16d321df6e4ad556424
Zan DIng <37390608+justinZan@users.noreply.github.com>
2024-04-09 17:08:19 +0800

    fix(tooltip): fix tooltip position issue (#66)
    * fix(tooltip): fix tooltip position issue
    
    * fix(tooltip): fix some comments

--- src/charts/tooltip/tooltip.types.ts
+++ src/charts/tooltip/tooltip.types.ts
@@ -16,14 +16,15 @@ export enum TooltipInteractionAxis {
 export interface TooltipOptions {
   useNative?: boolean;
   fontSize?: string;
-  content?: string;
   borderRadius?: string;
   maxWidth?: string;
   padding?: string;
   title?: string | string[] | ((tooltip: CJTooltipModel<CJUnknownChartType>) => string | string[]);
   formatTitle?: (title: string) => string;
+  beforeBody?: string | ((tooltip: CJTooltipModel<CJUnknownChartType>) => string);
   formatLabel?: (item: string) => string;
   formatValue?: (value: number) => string;
+  afterBody?: string | ((tooltip: CJTooltipModel<CJUnknownChartType>) => string);
   footer?: string | string[] | ((a: unknown) => string | string[]);
   showPercentage?: boolean;
   showUnit?: boolean;
