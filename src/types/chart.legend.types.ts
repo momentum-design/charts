4d3c72b013730848bfe9e82e81c773ff8be16fcb
Bendy Zhang <zb@bndy.net>
2024-04-26 10:09:43 +0800

    fix: resolve circular dependencies

--- src/types/chart.legend.types.ts
+++ src/types/chart.legend.types.ts
@@ -1,7 +1,7 @@
-import { TooltipOptions } from '../charts/tooltip';
-import { ChartEvent } from './chart.event.types';
-import { MarkerStyle, Position } from './chart.types';
-import { Font } from './font.types';
+import type { TooltipOptions } from '../charts/tooltip';
+import type { ChartEvent } from './chart.event.types';
+import type { MarkerStyle, Position } from './chart.types';
+import type { Font } from './font.types';
 
 export const inactiveColor = '#c9c9c9'; // TODO(bndynet): move to options
 export interface LegendItem {
